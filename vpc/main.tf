b059db3ad83b7d724db83889431e3c66322362bd
isow <ibrahim.sow@mutex-exterieur.fr>
2018-10-31 17:43:01 +0100

    commit 31 Octobre fin de journée

--- vpc/main.tf
+++ vpc/main.tf
@@ -191,20 +191,14 @@ resource "aws_security_group" "sg_bastion" {
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["37.173.134.0/32"]
   }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
   ingress {
     from_port   = 22
     to_port     = 22
